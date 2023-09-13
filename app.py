707d142865a71408d4325178ac8e05af1c9c3558
Husein Irfan <huseinirfan1@gmail.com>
2023-09-13 07:48:15 +0700

    add training page,  predctionPage in templates

--- app.py
+++ app.py
@@ -3,9 +3,16 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
+
+@app.route('/training')
+def trainingPage():
+    return render_template('trainingPage.html')
+
+@app.route('/prediction')
+def predictionPage():
+    return render_template('predictionPage.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
