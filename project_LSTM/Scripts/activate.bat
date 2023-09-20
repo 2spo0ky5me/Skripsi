72fe39198254eae74c60ea9a6e79a878add7ea4c
Husein Irfan <huseinirfan1@gmail.com>
2023-09-20 09:24:12 +0700

    fix FIX venv thank god

--- project_LSTM/Scripts/activate.bat
+++ project_LSTM/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Bill\Documents\Kuliah\project_LSTM
+set VIRTUAL_ENV=C:\Users\Bill\Documents\Kuliah\Skripsi\project_LSTM
 
 if not defined PROMPT set PROMPT=$P$G
 
